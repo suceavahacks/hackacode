1a48223a6bdcfbfa4d234edf9f6692d45435b57c
Sofian Adelin <adelinmihai071@gmail.com>
2025-05-12 08:27:49 +0000

    deployment bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-import { motion, useInView } from "framer-motion";
 import { useRef } from "react";
 import { useUser } from "@/utils/queries/user/getUser";
 import { useRouter } from "next/navigation";
@@ -13,13 +12,14 @@ export default function Home() {
   const refWaiting = useRef<HTMLDivElement>(null);
   const refAbout = useRef<HTMLDivElement>(null);
 
-  const isInViewLaptop = useInView(refLaptop, { once: true });
-  const isInViewLaptopText = useInView(refLaptopText, { once: true });
-  const isInViewWaiting = useInView(refWaiting, { once: true });
-  const isInViewAbout = useInView(refAbout, { once: true });
+  //const isInViewLaptop = useInView(refLaptop, { once: true });
+  //const isInViewLaptopText = useInView(refLaptopText, { once: true });
+  //const isInViewWaiting = useInView(refWaiting, { once: true });
+  //const isInViewAbout = useInView(refAbout, { once: true });
 
   const { user, loading, error } = useUser();
   const router = useRouter();
+
   useEffect(() => {
     if (user) {
       router.push("/app");
@@ -28,6 +28,11 @@ export default function Home() {
 
   if (loading) {
     return <Loading />;
+  }
+
+  if(user) {
+    router.push("/app");
+    return null;
   }
 
 
