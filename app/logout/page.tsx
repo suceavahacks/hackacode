be1809d4c61a9bd48bc08716b6ef85a4d6316620
Sofian Adelin <adelinmihai071@gmail.com>
2025-05-12 08:20:39 +0000

    fix deployment error

--- app/logout/page.tsx
+++ app/logout/page.tsx
@@ -1,25 +1,25 @@
-"use client"
+    "use client"
 
-import { useEffect } from "react"
+    import { useEffect } from "react"
 
-const Logout = () => {
+    const Logout = () => {
 
-    useEffect(() => {
-        if (typeof window !== "undefined") {
-            window.localStorage.removeItem("access_token")
-            window.localStorage.removeItem("refresh_token")
-            window.localStorage.removeItem("expires_at")
-        }
-    }, [])
+        useEffect(() => {
+            if (typeof window !== "undefined") {
+                window.localStorage.removeItem("access_token")
+                window.localStorage.removeItem("refresh_token")
+                window.localStorage.removeItem("expires_at")
+                window.location.href = "/"
+            }
+        }, [])
 
-    window.location.href = "/"
-    return (
-        <div className="w-screen flex flex-col items-center justify-center gap-10 min-h-screen max-md:mt-40">
-            <h1 className="text-7xl font-extrabold text-center max-md:text-5xl">You have been logged out</h1>
-            <p className="text-xl">Redirecting...</p>
-        </div>
-    )
+        return (
+            <div className="w-screen flex flex-col items-center justify-center gap-10 min-h-screen max-md:mt-40">
+                <h1 className="text-7xl font-extrabold text-center max-md:text-5xl">You have been logged out</h1>
+                <p className="text-xl">Redirecting...</p>
+            </div>
+        )
 
-}
+    }
 
-export default Logout
+    export default Logout
