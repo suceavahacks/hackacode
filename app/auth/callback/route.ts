99dfef506f5b05b440d400baf0a8c3a974bdedda
Sofian Adelin <adelinmihai071@gmail.com>
2025-05-13 17:28:07 +0000

    google oauth

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -2,5 +2,13 @@ import { createClient } from "@/utils/supabase/server";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  
+    const { searchParams } = new URL(request.url);
+    const access_token = searchParams.get("access_token");
+    const refresh_token = searchParams.get("refresh_token");
+    const expires_at = searchParams.get("expires_at");
+
+    localStorage.setItem("access_token", access_token || "");
+    localStorage.setItem("refresh_token", refresh_token || "");
+    localStorage.setItem("expires_at", expires_at || "");
+
 }
