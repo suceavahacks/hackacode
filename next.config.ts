71e3d5855639dab252bdcec5cea40e4c9257095e
Sofian Adelin <adelinmihai071@gmail.com>
2025-05-13 18:45:59 +0000

    fix homepage issue for authenticated users

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: [
       "hc-cdn.hel1.your-objectstorage.com", "ca.slack-edge.com"],
