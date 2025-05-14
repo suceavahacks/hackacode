048b56f2d5e2b799c786be4333a0a1eaad073913
Sofian Adelin <adelinmihai071@gmail.com>
2025-05-14 13:12:47 +0000

    Possible fix for undefined redirect.

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -16,7 +16,7 @@ export async function GET(request: Request) {
   }
 
   if (redirectTo) {
-    return NextResponse.redirect(`${origin}${redirectTo}`);
+    return NextResponse.redirect(`${process.env.NEXT_PUBLIC_URL}/app`);
   }
 
   // URL to redirect to after sign up process completes
