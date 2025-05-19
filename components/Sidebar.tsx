6aac3ed0ca864813bd572f427933fd8d45535c10
Sofian Adelin <adelinmihai071@gmail.com>
2025-05-19 08:29:38 +0000

    Sidebar high z-index

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -26,7 +26,7 @@ const Sidebar = () => {
     return (
         <div 
             className="sidebar fixed top-0 left-0 h-full transition-all duration-300 ease-in-out shadow-lg bg-secondary z-50 max-md:hidden"
-            style={{ width: isHovered ? '200px' : '64px' }}
+            style={{ width: isHovered ? '200px' : '64px', zIndex: 1000 }}
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
         >
