8efbd5ad4e14b8f8d75185211c746ea538c5e85a
Adi <111001628+xndadelin@users.noreply.github.com>
2025-07-13 18:55:44 -0700

    Update googleAuth.ts

--- utils/mutations/user/oauth/googleAuth.ts
+++ utils/mutations/user/oauth/googleAuth.ts
@@ -3,10 +3,7 @@ import { createClient } from "@/utils/supabase/client";
 const googleAuth = async () => {
     const supabase = createClient();
     const { data, error } = await supabase.auth.signInWithOAuth({
-        provider: "google",
-        options: {
-            redirectTo: `${window.location.origin}/auth/callback`,
-        },
+        provider: "google"
     });
 
     return data;
