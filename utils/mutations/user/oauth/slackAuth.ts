58177e0cd83dc859fe5ec5c7dc3a7d0024b90a50
Adi <111001628+xndadelin@users.noreply.github.com>
2025-07-13 18:55:58 -0700

    Update slackAuth.ts

--- utils/mutations/user/oauth/slackAuth.ts
+++ utils/mutations/user/oauth/slackAuth.ts
@@ -3,10 +3,7 @@ import { createClient } from "@/utils/supabase/client";
 const slackAuth = async () => {
     const supabase = createClient();
     const { data, error } = await supabase.auth.signInWithOAuth({
-        provider: "slack_oidc",
-        options: {
-            redirectTo: `${window.location.origin}/auth/callback`,
-        },
+        provider: "slack_oidc"
     });
 
     return data;
