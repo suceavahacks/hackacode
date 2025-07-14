459fe558ada0e81f2f65138294ccad3f970eb951
Adi <111001628+xndadelin@users.noreply.github.com>
2025-07-13 18:55:29 -0700

    Update githubAuth.ts

--- utils/mutations/user/oauth/githubAuth.ts
+++ utils/mutations/user/oauth/githubAuth.ts
@@ -3,10 +3,7 @@ import { createClient } from "@/utils/supabase/client";
 const githubAuth = async () => {
     const supabase = createClient();
     const { data, error } = await supabase.auth.signInWithOAuth({
-        provider: "github",
-        options: {
-            redirectTo: `${window.location.origin}/auth/callback`,
-        },
+        provider: "github"
     });
 
     return data;
